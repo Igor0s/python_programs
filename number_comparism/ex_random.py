1744c010f9d639e17f0e113e781e7cdc394d8b69
Igor0s <igi115511@gmail.com>
2023-01-24 09:05:50 +0100

    Update ex_random.py

--- number_comparism/ex_random.py
+++ number_comparism/ex_random.py
@@ -1,8 +1,12 @@
-number1 = int(input("Enter a number: "))
-number2 = int(input("Enter a number: "))
-if number1 > number2:
-    print(f"The number {number1} is greater than {number2}")
-elif number1< number2:
-    print(f"The number {number2} is greater than {number1}")
+try:   
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter a number: "))
+except ValueError:
+    print("some of entered values does not seem like integer")
 else:
-    print("Numbers are equal")
+    if number1 > number2:
+        print(f"The number {number1} is greater than {number2}")
+    elif number1< number2:
+        print(f"The number {number2} is greater than {number1}")
+    else:
+        print("Numbers are equal")
