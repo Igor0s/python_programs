38cd9ca1e2a829d8f8a89f4ebfff8b827d7679df
Igor0s <igi115511@gmail.com>
2023-01-24 08:44:08 +0100

    Create ex_random.py

---
+++ number_comparism/ex_random.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter a number: "))
+if number1 > number2:
+    print(f"The number {number1} is greater than {number2}")
+elif number1< number2:
+    print(f"The number {number2} is greater than {number1}")
+else:
+    print("Numbers are equal")
