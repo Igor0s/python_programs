3cf15dc3fd7ebaa924b03312d9b658150bf38de6
Igor0s <igi115511@gmail.com>
2024-01-25 23:25:12 +0100

    final

--- 5F/pexeso.py
+++ 5F/pexeso.py
@@ -1,15 +1,16 @@
 import tkinter
 import random
-import time
 
 def squares():
-    # function field creates visual interference of game
+    # function field creates visual interference of game,
+    #appends coordinates of each card in list coords
     x = 40
     y = 40
     number = 0
     for i in range(4):
         for i in range(4):
             canvas.create_rectangle(x,y, x+100, y+100, fill = "Grey")
+            square.append(number)
             coords.append([x,y])
             number += 1
             x += 125
@@ -17,36 +18,45 @@ def squares():
         x = 40
         
 def choose_square(event):
-    #function choose_square finds coordinates of square which user has clicked and stores them
-    #in variables x1,y1, x2.y2
-    global first_sq, second_sq, counter, x1, x2, y1, y2, score
+    #function choose_square finds coordinates, index and value of square which has user clicked and stores them
+    #in variables x1/2,y1/2 , index_sq1/2 , first_sq/second_sq
+    global first_sq, second_sq, counter, x1, x2, y1, y2, score, index_sq1, index_sq2
 
     for i in range(16):
         if event.x in range(coords[i][0], coords[i][0] + 100) and event.y in range(coords[i][1], coords[i][1] + 100):
             if counter == 0:
                 first_sq = values[i]
                 x1, y1 = coords[i]
+                index_sq1 = square[i]
+                #reveales card which user has clicked on
                 canvas.create_rectangle(x1, y1, x1 + 100, y1 + 100, fill="white")
                 canvas.create_text(x1 + 50, y1 + 50, text=str(values[i]))
             elif counter == 1:
                 second_sq = values[i]
                 x2, y2 = coords[i]
+                index_sq2 = square[i]
+                #reveales card which user has clicked on
                 canvas.create_rectangle(x2, y2, x2 + 100, y2 + 100, fill="white")
                 canvas.create_text(x2 + 50, y2 + 50, text=str(values[i]))
                 counter = 2
-
+    
     if counter >= 2:
         window.after(500, check_match)
     else:
         counter += 1
 
 def check_match():
-    global first_sq, second_sq, counter, x1, x2, y1, y2, score
+    global first_sq, second_sq, counter, x1, x2, y1, y2, score, index_sq1, index_sq2
     
-    if first_sq == second_sq and (x1 != x2 or y1 != y2):
+    #check if 2 revealed cards are matching
+    if first_sq == second_sq and index_sq1 != index_sq2:
         score += 1
         canvas.delete('score')
         canvas.create_text(WIDTH/2,10, text= f'SCORE: {score}/{len(values)//2}', tags = 'score', anchor= 'n', font = "Arial 15 bold italic")
+    elif x2 == 0:
+        canvas.create_rectangle(x1, y1, x1 + 100, y1 + 100, fill="grey")
+    elif x1 == 0:
+        canvas.create_rectangle(x2, y2, x2 + 100, y2 + 100, fill="grey")
     else:
         canvas.create_rectangle(x1, y1, x1 + 100, y1 + 100, fill="grey")
         canvas.create_rectangle(x2, y2, x2 + 100, y2 + 100, fill="grey")
@@ -62,8 +72,11 @@ HEIGHT = 550
 WIDTH = 550
 coords = []
 values = []
+square = []
 first_sq = 0
 second_sq = 0
+index_sq1 = 0
+index_sq2 = 0
 counter = 0
 x1 = 0
 x2 = 0
